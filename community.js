144d6f97108f8f910d98b6a56e81fc660db839da
Lucas-de-Lara-Biasi <lucas.biasi@escola.pr.gov.br>
2026-08-20 13:34:09 -0300

    Add files via upload

--- community.js
+++ community.js
@@ -1,9 +1,10 @@
-//Search method--------------------------------------------------------
+import { getRandomPFP } from "./profile.js"
 
+//SEARCH METHOD----------------------------------------------------------------------------------------------------------------
 const search_input = document.getElementById("search_input")
 const search_button = document.getElementById("search_button")
 
-function Search() {
+function FakeSearch() {
     let search_raw = search_input.value
     console.log("Searching... (' " + search_raw + " ')")
 
@@ -20,53 +21,9 @@ function Search() {
     //window.location.href = (window.location.href + "/search?" + search_raw)
 }
 
-search_button.addEventListener("click", Search)
+search_button.addEventListener("click", FakeSearch)
 
-//Create posts--------------------------------------------------------
-const post_template = document.getElementById("post_template")
-const posts_div = document.getElementById("posts_div")
-
-let post_counter_fake = 0;
-
-function CreatePost(title,author,content) {
-    let post = post_template.cloneNode(true)
-    post.id = post_counter_fake
-    post.style = ""
-
-    //regions
-    let post_header = post.getElementsByClassName("post_header")[0]
-    let post_content = post.getElementsByClassName("post_content")[0]
-    let post_footer = post.getElementsByClassName("post_footer")[0]
-
-    posts_div.appendChild(post)
-    post_counter_fake++;
-
-    //setup upvote / downvote
-    let post_votes = post_footer.getElementsByClassName("post_votes")[0]
-    let votes_up = post_votes.getElementsByClassName("post_vote_upvote")[0]
-    let votes_down = post_votes.getElementsByClassName("post_vote_downvote")[0]
-    let votes_text = post_votes.getElementsByClassName("post_votes_count")[0]
-
-    function UpdateText()  {
-        let count = 0
-        if (votes_up.checked) {
-            count+=1;
-        }
-        if (votes_down.checked) {
-            count-=1;
-        }
-        votes_text.textContent = count
-    }
-
-    votes_up.addEventListener("change",UpdateText)
-    votes_down.addEventListener("change",UpdateText)
-}
-
-for (let i = 0; i < 15; i++) {
-    CreatePost()
-}
-
-//time
+//TIME SET----------------------------------------------------------------------------------------------------------------
 function UpdateTime() {
     for (let time of document.getElementsByClassName("time")) {
         let computed_style = window.getComputedStyle(time)
@@ -76,19 +33,22 @@ function UpdateTime() {
         let formated = value
         let pos = "s"
 
-        if (value > 59) {
-            formated = Math.floor(value/60)
+        if (value > 59 && value <= 3599) {
+            formated = Math.floor(value / 60)
             pos = " min"
-        } else (value > 3599); {
-            formated = Math.floor(value/3600)
+        } else if (value > 3599 && value <= 86399) {
+            formated = Math.floor(value / 3600)
             pos = "h"
+        } else if (value > 86399) {
+            formated = Math.floor(value / 86400)
+            pos = "d"
         }
 
-        time.textContent = formated+pos
+        time.textContent = formated + pos
 
-        time.style.setProperty("--secs",value+1)
+        time.style.setProperty("--secs", value + 1)
     }
 }
 
 UpdateTime()
-setInterval(UpdateTime,1000)
+setInterval(UpdateTime, 1000)
